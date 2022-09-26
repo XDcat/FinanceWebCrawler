16e1a41f5f862f402b8dfc85647b4b35cab7c978
zzl <bulinbulinzzl@outlook.com>
2022-09-26 20:49:45 +0800

    更新common包

--- common/Logger.py
+++ common/Logger.py
@@ -1,5 +1,6 @@
 from loguru import logger
 
-logger.add("./log/info.log", format="{time} | {level} | {message}", filter="", level="INFO", rotation="50 MB")
-logger.add("./log/debug.log", filter="", level="DEBUG", rotation="10 MB")
-logger.add("./log/main.log", filter="__main__", level="DEBUG", rotation="100 MB")
+logger.add("./log/info.log", format="{time} | {level} | {message}", filter="", level="INFO", rotation="50 MB",
+           encoding='utf-8')
+logger.add("./log/debug.log", format="{time} | {level} | {message}", filter="", level="DEBUG", rotation="10 MB",
+           encoding='utf-8')
