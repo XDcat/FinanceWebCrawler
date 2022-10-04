777ca320f519ff079a20572151519f0ad3f31f36
514269236@qq.com <Zlj1784470039>
2022-10-04 22:22:57 +0800

    Test constructure

--- test/model/test_article.py
+++ test/model/test_article.py
@@ -14,6 +14,22 @@ class TestArticle:
             attachment="https://www.baidu.com"
         )
         article.save()
+        logger.info(article.body)
+        
         logger.info(article.ch_text)
+    def test_instance(self):
+        article = Article(
+            publish_date="09 01 2022",
+            body="<br>adadwa<br><p></p>", title="hello",
+            url="https://www.baidu.com",
+            author="me",
+            keyword="yae",
+            attachment="https://www.baidu.com"
+        )
+        article.save()
+        logger.info(article.body)
+        logger.info(article.ch_text)
+
+
         
         
