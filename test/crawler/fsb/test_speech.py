806032ff69621d1bd2eeaf0261610446d887044d
zzl <bulinbulinzzl@outlook.com>
2022-10-05 17:38:56 +0800

    测试fsb speech模块，通过部分文本

--- test/crawler/fsb/test_speech.py
+++ test/crawler/fsb/test_speech.py
@@ -8,5 +8,5 @@ class TestWorkingPaper(TestCase):
         self.runner = FSBSpeechesRunner()
 
     def test_run(self):
-        res = self.runner.run(file_name="output_speech.txt")
+        res = self.runner.run(end_at=4)
         logger.info(res)
