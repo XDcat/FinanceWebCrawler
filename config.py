9eb8598a3e7d581057227697d057f6060dc57b1e
zzl <bulinbulinzzl@outlook.com>
2022-10-06 21:18:21 +0800

    数据库文件更名

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import peewee
 import os
 
-db_path = os.path.join(os.path.dirname(__file__), "database/test.db")
+db_path = os.path.join(os.path.dirname(__file__), "database/FinanceWebCrawler.db")
 
 db = peewee.SqliteDatabase(db_path)
