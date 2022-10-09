8f1b681bef0e83a5e8bac557074393561b4a1962
reallyhandsomeman <1415510336@qq.com>
2022-10-09 19:55:59 +0800

    使用main函数保存文档

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from controller.article_selector import ArticleSelector
 from view.viewer import ArticleViewer
 
 def main():
-    arts = ArticleSelector.select_arts_from_db("2022-09-01", "2022-09-30",website="ECB")
+    arts = ArticleSelector.select_arts_from_db("2022-05-01", "2022-09-30")
     for art in arts:
         vw = ArticleViewer(art)
         vw.publish_en_report()
