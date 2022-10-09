6626f9a581fef06d881b777ba52f896840f5683b
reallyhandsomeman <1415510336@qq.com>
2022-10-09 19:13:47 +0800

    add report save path

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import peewee
 import os
 
 db_path = os.path.join(os.path.dirname(__file__), "database/FinanceWebCrawler.db")
-
+report_prefix_path = os.path.join(os.path.dirname(__file__), "results/")
 db = peewee.SqliteDatabase(db_path)
