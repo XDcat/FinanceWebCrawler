0447982a8e534dcb56ae08e97c2526b1086b7bd9
reallyhandsomeman <1415510336@qq.com>
2022-10-09 19:14:00 +0800

    add main

---
+++ main.py
@@ -0,0 +1,13 @@
+from controller.article_selector import ArticleSelector
+from view.viewer import ArticleViewer
+
+def main():
+    arts = ArticleSelector.select_arts_from_db("2022-09-01", "2022-09-30",website="ECB")
+    for art in arts:
+        vw = ArticleViewer(art)
+        vw.publish_en_report()
+
+
+
+if __name__ == '__main__':
+    main()
