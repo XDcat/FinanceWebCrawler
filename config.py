53685e5c2fa6fd93fbc9f0c45bf3f4c660e1b987
reallyhandsomeman <1415510336@qq.com>
2022-10-14 15:41:27 +0800

    修改config

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ import os
 db_path = os.path.join(os.path.dirname(__file__), "database/FinanceWebCrawler.db")
 report_prefix_path = os.path.join(os.path.dirname(__file__), "results/")
 db = peewee.SqliteDatabase(db_path)
+appid = "20191016000342020"
+password = "FERT7SoJhp0WagCy2Gz3"
