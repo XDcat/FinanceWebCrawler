dd46fe503bcfb9343dde238da662842a3214255c
reallyhandsomeman <1415510336@qq.com>
2022-10-21 13:49:07 +0800

    使用typer完成main函数功能

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from controller.article_controller import ArticleController
-from view.viewer import ArticleViewer
+import typer
+app=typer.Typer()
 
-def main():
-    arts = ArticleController.select_arts_from_db("2022-09-01", "2022-09-30")
-    for art in arts:
-        vw = ArticleViewer(art)
-        vw.publish_en_report()
+@app.command()
+def getLastMonthArticle():
+    """
+    获取上个月的文章
+    :return:
+    """
+    ArticleController.generate_last_month_articles()
 
 
+@app.command()
+def upDateDatabase(afterDate):
+    """
+    更新afterDate日期以后的文章，导入数据，afterDate 按照"YY-MM-DD"格式输入
+    :param afterDate:
+    :return:
+    """
+    ArticleController.update_db(after_date=afterDate)
 
-if __name__ == '__main__':
-    main()
+@app.command()
+def getArticles(start_from , end_at):
+    """
+    按照指定日期区间展示文章
+    :param start_from: 开始时间
+    :param end_at: 结束时间
+    :return:
+    """
+    ArticleController.view_article(start_date=start_from, end_date=end_at)
+
+
+if __name__ == "__main__":
+    app()
