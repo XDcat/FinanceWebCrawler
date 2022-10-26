63ff55d6c6571f374974e3e492bf80e7df7cd8a7
zzl <bulinbulinzzl@outlook.com>
2022-10-26 17:33:16 +0800

    main函数修改格式和命名

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from controller.article_controller import ArticleController
 import typer
-app=typer.Typer()
+
+app = typer.Typer()
+
 
 @app.command()
-def getLastMonthArticle():
+def get_last_month_articles():
     """
     获取上个月的文章
-    :return:
     """
     ArticleController.generate_last_month_articles()
 
 
 @app.command()
-def upDateDatabase(afterDate):
+def update_database(afterDate):
     """
     更新afterDate日期以后的文章，导入数据，afterDate 按照"YY-MM-DD"格式输入
-    :param afterDate:
-    :return:
     """
     ArticleController.update_db(after_date=afterDate)
 
+
 @app.command()
-def getArticles(start_from , end_at):
+def get_articles(start_from, end_at):
     """
     按照指定日期区间展示文章
     :param start_from: 开始时间
     :param end_at: 结束时间
-    :return:
     """
     ArticleController.view_article(start_date=start_from, end_date=end_at)
 
